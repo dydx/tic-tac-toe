438e9699e0ebab2e071aa948e9a8ac6cf4aeb7ff
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-11 11:52:19 -0500

    game board is designed

---
+++ script.js
@@ -0,0 +1,18 @@
+var x_moves = []
+var o_moves = []
+
+// need a method via jQuery that gets the `data-cell` # from a clicked
+// element and push it into the correct player's array
+
+// need to be able to handle whos move it is. we can do this by
+// incremementing a counter from 1 to n, and if the counter is
+// odd, its player one's move... even, player two's move
+
+
+// playerState = 1
+// while playGame {
+//
+// }
+
+
+// need to be able to insert the right icon when a player makes their move
